9842a82db1f6efd01e3d499fbcc6f93f23f4f758
Amlan2024 <159483085+Amlan2024@users.noreply.github.com>
2024-03-03 20:17:52 +0530

    Delete .vscode-server directory

--- .vscode-server/data/User/History/-623e6454/Bdr7.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-// SPDX-Licence-Identifier:MIT
-pragma solidity 0.8.18;
-
-contract DeplySimpleStorage{
-
-}
--- .vscode-server/data/User/History/-623e6454/qqXX.sol
+++
@@ -1,6 +0,0 @@
-SPDX-Licence-Identifier:MIT
-pragma solidity 0.8.18;
-// 
-contract DeplySimpleStorage{
-
-}
